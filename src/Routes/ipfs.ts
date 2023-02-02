8cb64f1334bc715c9770b275619e7531f280ea30
Uğur Eren <contact@ugureren.net>
2023-02-02 18:01:49 +0300

    IPFS Upload

--- src/Routes/ipfs.ts
+++ src/Routes/ipfs.ts
@@ -1,12 +1,41 @@
 import {join} from 'node:path';
 import {createReadStream} from 'node:fs';
 import express from 'express';
+import fetch from 'node-fetch';
 import HTTPStatus from '../Utils/HTTPStatus';
-import {IPFS_API_URL, IPFS_FOLDER} from '../Utils/Constants';
+import {IPFS_API_URL, IPFS_FOLDER, PINATA_API_KEY, PINATA_SECRET_API_KEY} from '../Utils/Constants';
 import {FileDownloader, FileExists} from '../Utils/Helpers';
 import IpfsState from '../Utils/IpfsState';
 
 const Router = express.Router();
+
+Router.post('/', async (req, res) => {
+  const {fileName, data} = req.body;
+
+  const content = JSON.stringify({
+    pinataOptions: {
+      cidVersion: 1,
+    },
+    pinataMetadata: {
+      name: fileName,
+    },
+    pinataContent: data,
+  });
+
+  const response = await fetch('https://api.pinata.cloud/pinning/pinJSONToIPFS', {
+    method: 'POST',
+    body: content,
+    headers: {
+      'Content-Type': 'application/json',
+      pinata_api_key: PINATA_API_KEY,
+      pinata_secret_api_key: PINATA_SECRET_API_KEY,
+    },
+  });
+
+  const responseJSON = await response.json();
+
+  res.status(HTTPStatus.OK).send(responseJSON);
+});
 
 Router.get('/:hash', async (req, res) => {
   const {hash} = req.params;
